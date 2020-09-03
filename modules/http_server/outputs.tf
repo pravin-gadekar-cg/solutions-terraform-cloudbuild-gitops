40790f112a6ceff505ca71183596ef6ace187bb2
Pravin Gadekar <pravin.gadekar@capgemini.com>
2020-09-03 13:42:01 +0000

    terraform fix

--- modules/http_server/outputs.tf
+++ modules/http_server/outputs.tf
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 
 output "instance_name" {
   value = "${google_compute_instance.http_server.name}"
