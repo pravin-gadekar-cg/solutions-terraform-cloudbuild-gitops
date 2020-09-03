1e80266fb8fc2eac6d4659bbad7b32a500ea9d09
Pravin Gadekar <pravin.gadekar@capgemini.com>
2020-09-03 12:14:47 +0000

    terraform fix

--- modules/notebook/main.tf
+++ modules/notebook/main.tf
@@ -1,8 +1,7 @@
 
 
 resource "google_notebooks_instance" "instance" {
-  provider = google-beta
-  name = "notebooks-instance"
+  name = "notebooks-instance-terraform"
   location = "us-west1-a"
   machine_type = "n1-standard-1"
   vm_image {
