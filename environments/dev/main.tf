6cefc2c0f1c05b50627c6bc5cca4d942752a24eb
Pravin Gadekar <pravin.gadekar@capgemini.com>
2020-09-03 13:01:33 +0000

    terraform fix

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,35 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#project = "${var.project}" --Pravin
 
 locals {
   env = "dev"
 }
 
 provider "google-beta" {
-  project = "${var.project}"
+  project = var.project
 }
 
 module "vpc" {
   source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
+  project = var.project
+  env     = local.env
 }
 
 module "http_server" {
   source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
+  project = var.project
+  subnet  = module.vpc.subnet
 }
 
 module "firewall" {
   source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
+  project = var.project
+  subnet  = module.vpc.subnet
 }
 
 module "notebook" {
   source  = "../../modules/notebook"
-  project = "${var.project}"
-  env     = "${local.env}"
+  project = var.project
+  env     = local.env
 }
