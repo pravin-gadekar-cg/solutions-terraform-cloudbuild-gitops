fa33dae959c99101e2ea5099cb3d323e0c5a8562
Pravin Gadekar <pravin.gadekar@capgemini.com>
2020-09-03 11:43:38 +0000

    terraform fix

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -38,19 +38,3 @@ module "firewall" {
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
 }
-
-
-resource "google_notebooks_instance" "instance" {
-  provider = google-beta
-  name = "kfp-notebooks-instance-terraform"
-  location = "us-west1-a"
-  machine_type = "n1-standard-1"
-  metadata = {
-    proxy-mode = "service_account"
-    terraform  = "true"
-  }
-  container_image {
-    repository = "gcr.io/deeplearning-platform-release/base-cpu"
-    tag = "latest"
-  }
-}
