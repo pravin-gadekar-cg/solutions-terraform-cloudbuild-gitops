60d54e6919a453c3647e695fcfc81c713546b38b
Pravin Gadekar <pravin.gadekar@capgemini.com>
2020-09-03 13:28:21 +0000

    terraform fix

--- modules/notebook/main.tf
+++ modules/notebook/main.tf
@@ -1,17 +1,15 @@
-
-
-resource “google_notebooks_instance” “instance” {
+resource "google_notebooks_instance" "instance" {
   provider = google-beta
-  name = “notebook-terraform-1”
+  name = "notebook-terraform-1"
   location = "us-west1-a"
-  machine_type = “n1-standard-1”
+  machine_type = "n1-standard-1"
   install_gpu_driver = true
   accelerator_config {
-  type = “NVIDIA_TESLA_T4”
+  type = "NVIDIA_TESLA_T4"
   core_count = 1
   }
   vm_image {
   project = var.project
-  image_family = “tf-latest-gpu”
+  image_family = "tf-latest-gpu"
   }
 }
