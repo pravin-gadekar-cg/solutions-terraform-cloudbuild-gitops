d7dd8168a5705ed51285060c72e2257dba1eec12
Pravin Gadekar <pravin.gadekar@capgemini.com>
2020-09-03 13:13:37 +0000

    terraform fix

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,23 +22,6 @@ provider "google-beta" {
   project = var.project
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = var.project
-  env     = local.env
-}
-
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = var.project
-  subnet  = module.vpc.subnet
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = var.project
-  subnet  = module.vpc.subnet
-}
 
 module "notebook" {
   source  = "../../modules/notebook"
