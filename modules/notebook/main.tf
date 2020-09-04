8f1eda253d4bafa34cc586c63006412a69926bf7
pravin-gadekar-cg <70685893+pravin-gadekar-cg@users.noreply.github.com>
2020-09-04 19:49:06 +0530

    Update main.tf
    Thiru Updates for AI Notebook Creation for "User 1"

--- modules/notebook/main.tf
+++ modules/notebook/main.tf
@@ -3,18 +3,18 @@ resource "random_id" "instance_id" {
  byte_length = 8
 }
 
-# resource "google_notebooks_instance" "instance" {
-  # provider = google-beta
-  # name = "ab-${random_id.instance_id.hex}"
-  # location = "us-west1-a"
-  # machine_type = "n1-standard-1"
-  # install_gpu_driver = true
-  # accelerator_config {
-  # type = "NVIDIA_TESLA_T4"
-  # core_count = 1
-  # }
-  # vm_image {
-  # project      = "deeplearning-platform-release"
-  # image_family = "tf-latest-gpu"
-  # }
-# }
+resource "google_notebooks_instance" "instance" {
+  provider = google-beta
+  name = "ab-${random_id.instance_id.hex}"
+  location = "us-west1-a"
+  machine_type = "n1-standard-1"
+  install_gpu_driver = true
+  accelerator_config {
+  type = "NVIDIA_TESLA_T4"
+  core_count = 1
+  }
+  vm_image {
+  project      = "deeplearning-platform-release"
+  image_family = "tf-latest-gpu"
+   }
+ }
